9fdec8fad9676d88fc4a69abd50336cd44a0ab6f
Hugues Lismonde <hugues@epic.net>
2022-01-31 22:20:19 +0100

    fix: 🐛 remove NODE_PATH on deactivate and set correct path

--- functions/_nvm_version_activate.fish
+++ functions/_nvm_version_activate.fish
@@ -1,5 +1,5 @@
 function _nvm_version_activate --argument-names v
     set --global --export nvm_current_version $v
     set --prepend PATH $nvm_data/$v/bin
-    set --global --export NODE_PATH $nvm_data/$v/node_modules
+    set --global --export NODE_PATH $nvm_data/$v/lib/node_modules
 end
--- functions/_nvm_version_deactivate.fish
+++ functions/_nvm_version_deactivate.fish
@@ -1,5 +1,6 @@
 function _nvm_version_deactivate --argument-names v
     test "$nvm_current_version" = "$v" && set --erase nvm_current_version
     set --local index (contains --index -- $nvm_data/$v/bin $PATH) &&
-        set --erase PATH[$index]
+    set --erase PATH[$index]
+    set --erase NODE_PATH
 end
